74a07e85908b616e09e70aa99eefadfc46b9c988
Trent Schneider <trentonschneider@gmail.com>
2020-09-23 16:44:26 -0400

    added initial routes

---
+++ routes/routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+// home page
+router.get("/", (req, res) => {});
+// exercise page
+router.get("/exercise", (req, res) => {});
+// stats page
+router.get("/stats", (req, res) => {});
+router
+  .route("/api/workout")
+  //   get last workout
+  .get((req, res) => {})
+  //   create workout
+  .post((req, res) => {});
+//   get workouts in range
+router.get("/api/workouts/range", (req, res) => {});
+// add exercise
+router.post("/api/workouts/:id", (req, res) => {});
