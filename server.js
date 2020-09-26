1d489259e21225a1a0aeab549479e267d3867403
Trent Schneider <trentonschneider@gmail.com>
2020-09-25 22:11:44 -0400

    added heroku db connection

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  userUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(require("./routes/routes"))
